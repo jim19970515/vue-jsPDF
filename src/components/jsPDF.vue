1e98988460a548aed6c5dec251e79394cb10c550
jim <f0920515972@gmail.com>
2025-05-03 14:56:27 +0800

    feat:新增 PDF中文字體

--- src/components/jsPDF.vue
+++ src/components/jsPDF.vue
@@ -1,10 +1,23 @@
 <script setup>
 import { jsPDF } from "jspdf";
-const doc = new jsPDF
+import autoTable from 'jspdf-autotable';
+import '../assets/pdf-font/msjh'// 引入字體定義檔
+const doc = new jsPDF()
 
+doc.setFont('msjh')// 使用註冊的字體名稱
+
+
+const savePdf = () =>{
+  doc.setFontSize(20);
+  doc.text("出餐單", 10, 10);
+  doc.setFontSize(12);
+  doc.text(`桌號：A3`, 10, 20);
+  doc.text(`時間：2025/05/01 12:34`, 10, 30);
+  doc.save('出餐單.pdf');
+}
 </script>
 
 <template>
-
+  <button @click="savePdf">儲存Pdf</button>
 </template>
 
